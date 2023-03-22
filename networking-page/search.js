f8fe88dcc9c585783a26b9cb5a0a04dd1702b838
khvicha <xvicha04@gmail.com>
2023-03-22 23:19:51 +0400

    update page part 2

--- networking-page/search.js
+++ networking-page/search.js
@@ -1,7 +1,11 @@
 
 const icon = document.querySelector('.icon');
 const search = document.querySelector('.search');
+const clear = document.querySelector('.clear');
 icon.onclick = function () {
+    search.classList.toggle('active')
+}
+clear.onclick = function () {
     search.classList.toggle('active')
 }
 
